8033c8056c4fbc7b669aa1e1bbced756bc9d02d1
gered <gered@blarg.ca>
2012-12-18 12:30:20 -0500

    add operator== to Vector2

--- src/geometry/vector2.h
+++ src/geometry/vector2.h
@@ -8,4 +8,12 @@ public:
 	float y;
 };
 
+bool operator==(const Vector2 &left, const Vector2 &right);
+
+inline bool operator==(const Vector2 &left, const Vector2 &right)
+{
+	return (left.x == right.x && left.y == right.y);
+}
+
+
 #endif
