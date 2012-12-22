32c9c8e777cd2adcc179fb6983d1a535655bec69
gered <gered@blarg.ca>
2012-12-22 18:06:51 -0500

    add alpha and texture fields to the Group chunk struct

--- src/geometry/group.h
+++ src/geometry/group.h
@@ -7,6 +7,8 @@
 struct Group
 {
 	std::string name;
+	std::string texture;
+	uint8_t alpha;
 	uint32_t numTriangles;
 
 	uint32_t GetSize() const;
@@ -17,6 +19,8 @@ inline uint32_t Group::GetSize() const
 	uint32_t size = 0;
 
 	size += sizeof(char) * (name.size() + 1);       // name string (including null-terminator)
+	size += sizeof(char) * (texture.size() + 1);    // texture file (including null-terminator)
+	size += sizeof(uint8_t);                        // alpha flag (boolean)
 	size += sizeof(uint32_t);                       // number of triangles
 
 	return size;
