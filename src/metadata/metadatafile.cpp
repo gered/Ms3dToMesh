b4ac0d07d3cc471c7d08db725f7da2beae9139c6
gered <gered@blarg.ca>
2012-12-22 16:22:02 -0500

    add stub MetadataFile class

---
+++ src/metadata/metadatafile.cpp
@@ -0,0 +1,20 @@
+#include "metadatafile.h"
+
+#include "../common.h"
+#include "../animationsequence.h"
+#include "../geometry/extragroupinfo.h"
+#include <string>
+#include <vector>
+
+MetadataFile::MetadataFile()
+{
+}
+
+void MetadataFile::Release()
+{
+}
+
+BOOL MetadataFile::Load(const std::string &file)
+{
+	return FALSE;
+}
---
+++ src/metadata/metadatafile.h
@@ -0,0 +1,30 @@
+#ifndef __METADATA_METADATAFILE_H_INCLUDED__
+#define __METADATA_METADATAFILE_H_INCLUDED__
+
+#include "../common.h"
+#include <string>
+#include <vector>
+
+struct AnimationSequence;
+struct ExtraGroupInfo;
+
+class MetadataFile
+{
+public:
+	MetadataFile();
+	virtual ~MetadataFile()                                { Release(); }
+	
+	void Release();
+	BOOL Load(const std::string &file);
+	
+	uint32_t GetNumAnimations() const                      { return m_animations.size(); }
+	const AnimationSequence* GetAnimations() const         { return &m_animations[0]; }
+	uint32_t GetNumGroupInfo() const                       { return m_groupInfo.size(); }
+	const ExtraGroupInfo* GetGroupInfo() const             { return &m_groupInfo[0]; }
+	
+private:
+	std::vector<AnimationSequence> m_animations;
+	std::vector<ExtraGroupInfo> m_groupInfo;
+};
+
+#endif
